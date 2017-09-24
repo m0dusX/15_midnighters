36e0b05baab3d53305b7b1874af683b51aced8f5
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-09-24 11:38:37 +0300

    Update seek_dev_nighters.py

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,34 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pendulum
 
-def get_midnighters():
-    pass
+def load_attempts():
+    start_parse = 1
+    end_parse = 11
+    url = "https://devman.org/api/challenges/solution_attempts"
+    for page in range(start_parse, end_parse):
+        payload = {"page" : page}
+        records = requests.get(url, params = payload).json()["records"]
+        for record in records:
+            timestamp = record["timestamp"]
+            if timestamp is not None:
+                yield {
+                    'username': record["username"],
+                    'timestamp': timestamp,
+                }
+
+def get_midnighters(attempts):
+    moscow_timezone = 'Europe/Moscow'
+    today_midnight = pendulum.today(moscow_timezone)
+    for attempt in attempts:
+        timestamp = attempt['timestamp']
+        if pendulum.from_timestamp(timestamp, moscow_timezone) > today_midnight:
+            yield {
+                'username': attempt['username'],
+                'time': pendulum.fromtimestamp(timestamp, moscow_timezone).to_datetime_string(),
+            }
 
 if __name__ == '__main__':
-  pass
+    counter = 0
+    for idx, midnighter in enumerate(get_midnighters(load_attempts()), 1):
+        print("{}) {} submitted task for review at {}\n".format(idx, 
+            midnighter['username'], midnighter['time']))
