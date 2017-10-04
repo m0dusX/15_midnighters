ff8ff961151d7d1759f5117e362a1d04367ef9b0
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-10-04 19:50:03 +0300

    Update seek_dev_nighters.py

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,13 +1,14 @@
 import requests
 import pendulum
 
+
 def load_attempts():
     start_parse = 1
     end_parse = 11
     url = "https://devman.org/api/challenges/solution_attempts"
     for page in range(start_parse, end_parse):
-        payload = {"page" : page}
-        records = requests.get(url, params = payload).json()["records"]
+        payload = {"page": page}
+        records = requests.get(url, params=payload).json()["records"]
         for record in records:
             timestamp = record["timestamp"]
             if timestamp is not None:
@@ -16,19 +17,25 @@ def load_attempts():
                     'timestamp': timestamp,
                 }
 
+
 def get_midnighters(attempts):
     moscow_timezone = 'Europe/Moscow'
     today_midnight = pendulum.today(moscow_timezone)
     for attempt in attempts:
-        timestamp = attempt['timestamp']
-        if pendulum.from_timestamp(timestamp, moscow_timezone) > today_midnight:
+        timestamp = attempt["timestamp"]
+        current_date = pendulum.from_timestamp(timestamp, moscow_timezone)
+        if current_date > today_midnight:
+            time = pendulum.fromtimestamp(timestamp, moscow_timezone)
+            time_human_view = time.to_datetime_string()
             yield {
-                'username': attempt['username'],
-                'time': pendulum.fromtimestamp(timestamp, moscow_timezone).to_datetime_string(),
+                'username': attempt["username"],
+                'time': time_human_view,
             }
+
 
 if __name__ == '__main__':
     counter = 0
     for idx, midnighter in enumerate(get_midnighters(load_attempts()), 1):
-        print("{}) {} submitted last task for review at {}\n".format(idx, 
-            midnighter['username'], midnighter['time']))
+        attempt_print = "{}) {} submitted task for review at {}\n"
+        print(attempt_print.format(idx, midnighter['username'],
+                                   midnighter['time']))
